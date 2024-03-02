c5fed82ccf6d030ad279a4d2e5051142d2e078cd
gabriel073 <gabrielmarzioli@hotmail.com>
2024-03-02 11:13:31 -0300

    Front terminado formulario funcionando

--- front/login/src/components/Login/Login.jsx
+++ front/login/src/components/Login/Login.jsx
@@ -1,8 +1,35 @@
-
+import { useState } from "react";
+import "../../login.css";
 
 function Login() {
+
+    const [password, setPassword] = useState('');
+    const [username, setUserName] = useState('');
+
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log({
+            username: username,
+            password: password
+        })
+    }
+
     return (
-        <div>Login....</div>
+        <>
+            <h1>Login</h1>
+            <div className="form">
+                <form>
+                    <label >Username:</label>
+                    <input onChange={(e) => { setUserName(e.target.value) }} placeholder="username" type="text" />
+
+                    <label >Password:</label>
+                    <input onChange={(e) => { setPassword(e.target.value) }} placeholder="password" type="password" />
+
+                    <button onClick={handleLogin}>Login</button>
+                </form>
+            </div>
+        </>
     )
 }
 
