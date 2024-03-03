9551b3b06feb40037d356f792178cdf90cb36ec4
gabriel073 <gabrielmarzioli@hotmail.com>
2024-03-03 18:51:02 -0300

    Revert "conectando front con back"
    This reverts commit bdd919506f9a0f3ef6ee404e39bf9c1b852d670c.

--- back/src/controllers/pingController.js
+++ back/src/controllers/pingController.js
@@ -1,16 +1,3 @@
-const connection = require('../models/db.js');
-
-
 module.exports.ping = (req, res) => {
-    const consult = 'select * from login';
-
-    try {
-        connection.query(consult, (error, results) => {
-            console.log(results)
-            res.json(results);
-        })
-    } catch (error) {
-        console.log(error)
-    }
-
+    res.send('this is a ping Test....')
 }
--- front/login/src/components/Login/Login.jsx
+++ front/login/src/components/Login/Login.jsx
@@ -9,25 +9,10 @@ function Login() {
 
     const handleLogin = (e) => {
         e.preventDefault();
-        const data = {
+        console.log({
             username: username,
             password: password
-        }
-
-        fetch('http://localhost:3000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
         })
-            .then(response => response.json())
-            .then(result => {
-                console.log((result))
-            })
-            .catch(err => {
-                console.log(err)
-            })
     }
 
     return (
