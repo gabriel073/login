492ec1ddd14ba29facbca7a644cba18a6c88344b
gabriel073 <gabrielmarzioli@hotmail.com>
2024-03-04 10:50:21 -0300

    resolviendo conexion front-back, solucionado

--- back/src/controllers/loginController.js
+++ back/src/controllers/loginController.js
@@ -2,24 +2,27 @@ const connection = require('../models/db.js')
 
 
 module.exports.login = (req, res) => {
+
+    const { username, password } = req.body;
+
+
+
+
+    const consult = 'SELECT * FROM login WHERE username = ? AND password = ?';
     try {
-        const { user } = req.body;
-        console.log('User: ', user);
+        connection.query(consult, [username, password], (err, result) => {
+            if (err) {
+                res.send(err);
+            }
+            if (result.length > 0) {
+                console.log(result)
+                res.send(result)
+            } else {
+                console.log('wrong user')
+                res.send({ message: 'wrong user' })
+            }
+        })
+    } catch (e) {
 
-    } catch (error) {
-        console.log(error)
     }
-
-    // try {
-    //     connection.query(consult, [username, password], (err, result) => {
-    //         if (err) {
-    //             res.send(err);
-    //         }
-
-
-    //     })
-    // } catch (e) {
-
-    // } 
-
 }
--- front/login/src/components/Login/Login.jsx
+++ front/login/src/components/Login/Login.jsx
@@ -12,8 +12,9 @@ function Login() {
         const data = {
             username: username,
             password: password
-        };
-        fetch("http://localhost:3000/login", {
+        }
+
+        fetch('http://localhost:3000/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
