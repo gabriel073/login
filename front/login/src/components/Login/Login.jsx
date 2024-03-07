3107fbcf8382dd7fac74c5e0530bebeb8e755bf6
gabriel073 <gabrielmarzioli@hotmail.com>
2024-03-07 17:52:01 -0300

    Estilando Form

--- front/login/src/components/Login/Login.jsx
+++ front/login/src/components/Login/Login.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import "../../login.css";
+import "../../Login.css";
 import Home from "../Home.jsx";
+// import Swal from 'sweetalert2';
 
 function Login() {
 
-    const [password, setPassword] = useState('');
     const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
     const [loginSuccessful, setLoginSuccessful] = useState(false);
+    const [userNameError, setUserNameError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
 
     const handleLogin = (e) => {
         e.preventDefault();
@@ -25,6 +28,14 @@ function Login() {
             .then(response => response.json())
             .then(result => {
                 console.log(result.token)
+                // if (setUserName != username) {
+                //     Swal.fire({
+                //         icon: "error",
+                //         title: "Error",
+                //         text: "User o Password Ingresados, Incorrectos!",
+
+                //     });
+                // }
 
                 if (result.token) {
                     localStorage.setItem('token', result.token)
@@ -32,33 +43,64 @@ function Login() {
 
                 } else {
                     setLoginSuccessful(false);
+
                 }
-                console.log(result)
 
             })
             .catch(error => {
                 console.log(error)
             })
+
+
+
+        if (!data.username) {
+            return setUserNameError('Ingrese un username');
+        }
+        if (!data.password) {
+            return setPasswordError('Ingrese un Password');
+        }
+        else {
+            return setUserNameError(false), setPasswordError(false);
+        }
+
+
+
     }
 
 
+
     return (
-        <>
-            {loginSuccessful ? <Home /> : <div className="custom-form">
-                <h1>Login</h1>
-                <div className="form">
+        <div >
+            {loginSuccessful ? <Home /> :
+                <div className='login-box'>
+                    <h2>Login</h2>
                     <form>
-                        <label >Username:</label>
-                        <input onChange={(e) => { setUserName(e.target.value) }} placeholder="username@mail.com" type="text" />
-
-                        <label >Password:</label>
-                        <input onChange={(e) => { setPassword(e.target.value) }} placeholder="xxxxxxxxx" type="password" />
-
-                        <button onClick={handleLogin}>Login</button>
+                        <div className='user-box'>
+                            <input onChange={(e) => { setUserName(e.target.value) }} name="" required="" type="text" />
+                            <div className='danger'>
+                                {userNameError && <p>{userNameError}</p>}
+                            </div>
+                            <label>Username</label>
+                        </div>
+                        <div className='user-box'>
+                            <input onChange={(e) => { setPassword(e.target.value) }} name="" required="" type="password" />
+                            <div className='danger'>
+                                {passwordError && <p>{passwordError}</p>}
+                            </div>
+                            <label >Password</label>
+                        </div>
+                        <div className="button">
+                            <button onClick={handleLogin}>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                Sing In
+                            </button>
+                        </div>
                     </form>
-                </div>
-            </div>}
-        </>
+                </div>}
+        </div>
     );
 }
 
